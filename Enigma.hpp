2ec8177782c70c69424e73feaf3e70f5fb8d19b7
RomainCocogne <romain.cocogne83@gmail.com>
2020-04-27 18:41:08 +0200

    classes key result escape_room

--- Enigma.hpp
+++ Enigma.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include "Result.hpp"
+#include "Key.hpp"
 
 class Virtual_Enigma {
  protected:
@@ -13,18 +15,18 @@ class Virtual_Enigma {
 template <typename K, R>
 class Enigma: Virtual_Enigma {
  private:
-    K key_;
-    R result_;
+    Key<K> key_;
+    Result<R> result_;
 
  public:
     Enigma(int k, int res): Virtual_Enigma(), key_{k}, result_{res} {}
     ~Enigma() = default;
 
-    void set_key(K k) { key_ = k;}
-    bool try_key(K k) {
+    void set_key(Key<K> k) { key_ = k;}
+    bool try_key(Key<K> k) {
         if (k == key_) return resolved_ = true;
         return false;
     }
     bool resolved() {return resolved_;}
-    R result() { return result_;}
+    Result<R> result() { return result_;}
 };

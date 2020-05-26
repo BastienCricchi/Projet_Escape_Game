d3e80733a85bb5e261faa2adf9010cbc6f165027
QuentinCombal <quentin.combal@hotmail.fr>
2020-05-26 18:22:56 +0200

    Implementation modele singleton pour Enigma

--- Enigma.hpp
+++ Enigma.hpp
@@ -1,6 +1,4 @@
 #pragma once
-// #include "Result.hpp"
-// #include "Key.hpp"
 
 class Virtual_Enigma {
  protected:
@@ -13,51 +11,96 @@ class Virtual_Enigma {
     bool resolved() {return resolved_;}
 };
 
-template <typename K,typename R>
+template <typename E, typename K,typename R>
 class Enigma: public Virtual_Enigma {
  private:
-    // Key<K> key_;
-    // Result<R> result_;
+    E riddle_;
     K key_;
     R result_;
  public:
-    Enigma(K k, R res): Virtual_Enigma(), key_{k}, result_{res} {}
+    Enigma(E riddle, K k, R res): Virtual_Enigma(), riddle_{riddle}, key_{k}, result_{res} {}
     ~Enigma() = default;
 
-    void set_key(K k) { key_ = k;}
+    //void set_key(K k) { key_ = k;}
+    
+    // Dans cette fonction, l'enigme devrait notifier ses observers du resultat
+    // -Q
     bool try_key(K k) {
         if (k == key_) return resolved_ = true;
         return false;
     }
 
-    R result() { return result_;}
+    E get_riddle(){ return riddle_;}
+
+    R get_result() { return result_;}
 };
 
 
-class Enigma_Textual: public Enigma<std::string,std::string>
+class Enigma_1_1 : public Enigma<std::string,std::string,std::string>
 {
 private:
-  std::string riddle_;
+  Enigma_1_1(): Enigma{
+    "Plus je suis chaud, plus je suis frais...\n Que suis-je ?",
+    "pain",
+    "Bien joué"
+  } {};
 
- public:
-  Enigma_Textual(std::string riddle, std::string key, std::string result):
-   Enigma(key, result),
-   riddle_{riddle}
-   {}
-  ~Enigma_Textual() = default;
-  
+  Enigma_1_1(Enigma_1_1 const&);              // Don't Implement
+  void operator=(Enigma_1_1 const&); // Don't implement
+
+public:
+  static Enigma_1_1* get_instance(){
+    static Enigma_1_1 instance;
+    return &instance;
+  }
 };
 
-Virtual_Enigma* enigma_text_1_1 = new Enigma_Textual(
-  "Plus je suis chaud, plus je suis frais...\n Que suis-je ?",
-  "pain",
-  "Bien joué"
-);
 
-Virtual_Enigma* enigma_text_2_1 = new Enigma_Textual(
-  "Mon premier se trouve en abondance dans la nourriture de fast food\n"
-  "Mon second est au milieu de ton visage\n"
-  "Mon tout est un professeur de C++",
-  "granet",
-  "Bien joué"
-);
+class Enigma_1_2 : public Enigma<std::string,std::string,std::string>
+{
+private:
+  Enigma_1_2(): Enigma{
+    "Mon premier se trouve en abondance dans la nourriture de fast food\n"
+    "Mon second est au milieu de ton visage\n"
+    "Mon tout est un professeur de C++",
+    "granet",
+    "Bien joué"
+  } {}
+
+  Enigma_1_2(Enigma_1_2 const&);     // Don't Implement
+  void operator=(Enigma_1_2 const&); // Don't implement
+
+public:
+  static Enigma_1_2* get_instance(){
+    static Enigma_1_2 instance;
+    return &instance;
+  }
+};
+
+// class Enigma_Textual: public Enigma<std::string,std::string>
+// {
+// private:
+//   std::string riddle_;
+
+//  public:
+//   Enigma_Textual(std::string riddle, std::string key, std::string result):
+//    Enigma(key, result),
+//    riddle_{riddle}
+//    {}
+//   ~Enigma_Textual() = default;
+  
+// };
+
+// Virtual_Enigma* enigma_text_1_1 = new Enigma_Textual(
+//   "Plus je suis chaud, plus je suis frais...\n Que suis-je ?",
+//   "pain",
+//   "Bien joué"
+// );
+
+// Virtual_Enigma* enigma_text_2_1 = new Enigma_Textual(
+//   "Mon premier se trouve en abondance dans la nourriture de fast food\n"
+//   "Mon second est au milieu de ton visage\n"
+//   "Mon tout est un professeur de C++",
+//   "granet",
+//   "Bien joué"
+// );
--- Escape_room.hpp
+++ Escape_room.hpp
@@ -4,22 +4,26 @@
 #include "Room.hpp"
 #define NUMBER_OF_ROOMS 3 
 
+
+Room *room1, *room2;
+
 class Escape_room {
  private:
-  std::vector<Room *> rooms_;
-  Room* current_room;
+  std::vector<Room> rooms_ ;
+  Room* current_room_;
  public:
   Escape_room(){
-
     std::vector<Virtual_Enigma *> enigmas_room_1 = {enigma_text_1_1};
     std::vector<Virtual_Enigma *> enigmas_room_2 = {enigma_text_2_1};
 
     rooms_.reserve(NUMBER_OF_ROOMS);
-    Room *room1, *room2;
+    rooms.emplace_back()
 
     room1 = new Room(room2, enigmas_room_1);
     room2 = new Room(NULL, enigmas_room_2);
 
   }
-  ~Escape_room();
+  ~Escape_room(){
+    delete room1,room2;
+  };
 };
